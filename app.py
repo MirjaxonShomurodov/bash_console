aba4b40e0c1c4e2bf2f1a808f2eb31189deddb19
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-01-26 08:51:32 +0500

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def add():
-    return "Hi Mirjakhan."
+    return "Shomurodov_Mirjakhan_2003"
 
 if __name__=="__main__":
     app.run(debug=True,port=4000)
